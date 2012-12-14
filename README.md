1ef36894f52f322085f5dfb14a1e2fb31bfaebed
Isra <isra00@gmail.com>
2012-12-14 17:15:39 +0100

    Write README and better examples

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
+Inject logic to any method of any PHP class, dinamically
+========================================================
+
 [![Build Status](https://secure.travis-ci.org/isra00/class-triggers.png)](http://travis-ci.org/isra00/class-triggers)
+
+This is a partial and experimental implementation of Aspect-Oriented Programming. It gives you the ability to capture and manipulate the inputs, outputs and execution of any method of any class in PHP.
+
+Basic usage
+-----------
+
+	$a = new AnyClass;
+	$a = new ClassTriggers($a);
+
+	$a->bind($targetMethod, $trigger, function($arguments) {
+		//Injected logic.
+	});
+
+
+Example
+-------
+
+	use ClassTriggers\ClassTriggers;
+
+	class Car
+	{
+		private $position = 0;
+
+		public function forward($increment)
+		{
+			$this->position += $increment;
+		}
+
+		public function getPosition()
+		{
+			return $this->position;
+		}
+	}
+
+	$interceptedCar = new ClassTriggers(new Car);
+	
+	$interceptedCar->bind('forward', 'preMethod', function(&$arguments) {
+		echo 'You have requested forward(' . $arguments[0] . ")\n";
+	
+		if ($this->position + $arguments[0] > 10) {
+			echo "Can't forward more than 10. Stopping method execution.\n";
+			return ClassTriggers::COND_STOP_EXECUTION;
+		}
+	});
+
+More info
+---------
+
+I wrote a tutorial in spanish:
+
+http://israelviana.es/inyectar-logica-en-los-metodos-de-cualquier-clase-php-dinamicamente/
+
+Requiremens
+-----------
+
+ * PHP 5.4
+ * Composer
+ 
+Installation
+------------
+
+You can install it via Composer, and use it instantly thanks to the Composer autoloader.
--- example.php
+++ example.php
@@ -4,18 +4,14 @@ include 'vendor/autoload.php';
 
 use ClassTriggers\ClassTriggers;
 
-class Coche {
+class Coche 
+{
 
 	private $posicion = 0;
 	
 	public function avanzar($incremento)
 	{
 		$this->posicion += $incremento;
-	}
-	
-	public function retroceder()
-	{
-		$this->posicion--;
 	}
 	
 	public function getPosicion()
@@ -30,17 +26,41 @@ $cocheInterceptado->bind('avanzar', 'preMethod', function(&$arguments) {
 	echo 'Has solicitado avanzar(' . $arguments[0] . ")\n";
 	
 	if ($this->posicion + $arguments[0] > 10) {
-		echo 'No puedes avanzar más de 10. Deteniendo ejecución del método';
+		echo "No puedes avanzar más de 10. Deteniendo ejecución del método\n";
 		return ClassTriggers::COND_STOP_EXECUTION;
 	}
 });
 
 
 $cocheInterceptado->bind('avanzar', 'postMethod', function(&$arguments, $output) {
-	echo "Has avanzado $arguments[0] -> " . $this->posicion . "\n";
+	if ($this->posicion > 10)
+	{
+		$this->posicion = 10;
+	}
 });
 
 $cocheInterceptado->avanzar(3);
+echo $cocheInterceptado->getPosicion() . "\n";
 $cocheInterceptado->avanzar(3);
+echo $cocheInterceptado->getPosicion() . "\n";
 $cocheInterceptado->avanzar(3);
+echo $cocheInterceptado->getPosicion() . "\n";
 $cocheInterceptado->avanzar(3);
+echo $cocheInterceptado->getPosicion() . "\n";
+
+$cocheInterceptado = new ClassTriggers(new Coche);
+
+$cocheInterceptado->bind('avanzar', 'postMethod', function(&$arguments) {
+	if ($this->posicion > 10)
+	{
+		$this->posicion = 10;
+	}
+	return ClassTriggers::NO_MORE_ACTIONS;
+});
+
+$cocheInterceptado->bind('avanzar', 'postMethod', function(&$arguments) {
+	$this->posicion = 99;
+});
+
+$cocheInterceptado->avanzar(11);
+echo $cocheInterceptado->getPosicion() . "\n";
